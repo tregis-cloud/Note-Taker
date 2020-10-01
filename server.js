56c1903aaf604d59fa1b98b354b4aa2708ff8836
Terrence Regis <terrence.regis@gmail.com>
2020-09-30 22:25:02 -0400

    Added boilerplate to server.js.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Dependences
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 //Setup Express APP
 const app = express();
@@ -10,10 +11,30 @@ const PORT = 7500;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// The following API routes should be created:
+const notes = [];
 
 //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(_dirname, "db.json"));
+});
 
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-
+app.post("/api/notes/:id", function (req, res) {
+  let newNote = req.body;
+  console.log(newNote);
+  notes.push(newNote);
+  res.json(newNote);
+});
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+app.delete("api/notes/:id", function () {
+  let chosen = req.params.id;
+  for (let i = 0; i < notes.length; i++) {
+    if (chosen === notes[i]) {
+      res.delete(notes[i]);
+    }
+  }
+});
+//Start server to begin listening
+app.listen(PORT, function () {
+  console.log("App is listening on PORT " + PORT);
+});
