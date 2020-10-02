3fd37826848f84914e5144a6014705593a3907ce
Terrence Regis <terrence.regis@gmail.com>
2020-10-02 16:22:12 -0400

    Testing and fixing issue.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -104,6 +104,7 @@ const handleRenderSaveBtn = function () {
 // Render's the list of note titles
 const renderNoteList = (notes) => {
   $noteList.empty();
+  console.log(notes);
 
   const noteListItems = [];
 
--- server.js
+++ server.js
@@ -1,40 +1,99 @@
+const { json } = require("body-parser");
 //Dependences
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+let db = require("./db.json");
 
 //Setup Express APP
 const app = express();
-const PORT = 7500;
+const PORT = process.env.PORT || 8080;
 
 //Setup express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-const notes = [];
+let filePath = path.join(__dirname + "/db.json");
 
-//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+// // GET Routes
 app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join(_dirname, "db.json"));
+  // fs.readFile(filePath, "utf-8", function (err, data) {
+  //   if (err) {
+  //     throw err;
+  //     return json.status(500).json({
+  //       error: true,
+  //       data: null,
+  //       message: "Unable to retrieve notes.",
+  //     });
+  //   }
+  //   res.json(JSON.parse(data));
+  // });
+  res.json(db);
 });
 
-//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-app.post("/api/notes/:id", function (req, res) {
-  let newNote = req.body;
-  console.log(newNote);
-  notes.push(newNote);
-  res.json(newNote);
-});
-//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-app.delete("api/notes/:id", function () {
-  let chosen = req.params.id;
-  for (let i = 0; i < notes.length; i++) {
-    if (chosen === notes[i]) {
-      res.delete(notes[i]);
-    }
+//POST Routes with error handling
+app.post("/api/notes", function (req, res) {
+  console.log("A", req.body);
+  db.push(req.body);
+
+  if (!req.body.title || !req.body.text) {
+    return res.status(400).json({
+      error: true,
+      data: null,
+      message: "Invalid note. Please try again.",
+    });
   }
+  fs.readFile(filePath, "utf-8", function (err, data) {
+    req.body.id = data.length;
+    console.log("D", data);
+    console.log("Z", req.body.id);
+    if (err) {
+      throw err;
+      return res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to retrieve notes.",
+      });
+    }
+    console.log("B", data);
+    const updatedNotes = JSON.parse(data);
+    console.log("C", updatedNotes);
+    updatedNotes.push(req.body);
+    console.log("D", updatedNotes);
+
+    fs.writeFile(filePath, JSON.stringify(updatedNotes), function (err) {
+      if (err) {
+        throw err;
+        return res.status(500).json({
+          error: true,
+          data: null,
+          message: "Unable to save new note.",
+        });
+      }
+      res.json({
+        error: false,
+        data: updatedNotes,
+        message: "Note successfully added.",
+      });
+    });
+  });
 });
+//DELETE `/api/notes/:id` -
+app.delete("/api/notes/:id", function (req, res) {
+  let id = req.params.id;
+  db = db.filter((note) => note.id != id);
+  res.json({ message: `note ${id} was deleted.` });
+});
+// View Routes
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 //Start server to begin listening
 app.listen(PORT, function () {
-  console.log("App is listening on PORT " + PORT);
+  console.log(`App is listening on http://localhost:${PORT}`);
 });
